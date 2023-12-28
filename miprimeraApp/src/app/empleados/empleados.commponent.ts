7a7a861ed34550d8c04dd100204c62c36742fcad
EdgardoCozzo <edgardocozzo@gmail.com>
2023-12-28 15:12:45 -0300

    base de los componentes

---
+++ miprimeraApp/src/app/empleados/empleados.commponent.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-empleado',
+    templateUrl: './empleados.component.html',
+    styleUrl: './empleados.component.css'
+  })
+export class EmpleadosComponent{}
