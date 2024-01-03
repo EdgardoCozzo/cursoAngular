8764f865b783564c33c668b3a0486db9a164e875
EdgardoCozzo <edgardocozzo@gmail.com>
2024-01-03 18:33:04 -0300

    video 10

--- miprimeraApp/src/app/empleado/empleado.component.ts
+++ miprimeraApp/src/app/empleado/empleado.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrl: './empleado.component.css'
 })
 export class EmpleadoComponent {
+nombre="Edgardo";
+apellido="Cozzo";
+edad=33;
+oli=false;
+usuRegistrado=false;
+
+llamaEmpresa(value:String){
+}
+
+getRegistroUsuario(){
+  this.usuRegistrado=true;
+}
+
+
+// empresa="quinto";
+
+// getEdad(){
+//   return this.edad;
+// }
 
 }
